1031226c79f7bc54d951ad25a3b773dbdea62e8c
pendulm <lonependulm@gmail.com>
2013-06-18 23:27:28 +0800

    auto archive

--- process.sh
+++ process.sh
@@ -1,6 +1,10 @@
 DIR=${1%%/}
+year=${DIR##hakm_teamblog_}
 # cat sql1.txt |sqlite3 $DIR/db > $DIR/result.txt
 mkdir -p $DIR/data
 # awk -f dispatch.awk -v DIR=$DIR $DIR/result.txt
 
 cat sql1.txt |sqlite3 $DIR/db | awk -f dispatch.awk -v DIR=$DIR
+mkdir -p archives
+tar -cvf - "$DIR/data" | gzip > "archives/$year.tar.gz"
+
